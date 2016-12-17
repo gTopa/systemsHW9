f793f6168c3d00038581314a9c20709d7369c0cb
plwslw <s.lit.winkler@gmail.com>
2016-12-17 18:20:01 -0500

    update

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,19 +1,35 @@
 #include "pipe_networking.h"
 
-int client_handshake(int *fd){
-  open(WKP);
-  char buff[12]="yestouching";
-  mkfifo(buff,0644);//change name maybe
-  write(WKP, buff, sizeof(buff));
+//returns from_server fd
+int client_handshake(int *to_server){
+  //2. create private pipe
+  int x=getpid();
+  char *buff;
+  sprintf(buff, "%d", x);
+  mkfifo(buff,0644);
+  int from_server = open(buff, O_RDONLY);
+  
+  //3. write name of private pipe to server
+  *to_server = open(WKP, O_WRONLY);
+  write(*to_server, buff, sizeof(buff));
+
+  //4. wait for confirm from server
+  char* output;
+  if (strcmp(read(fd_priv, output, strlen(SERVER_CONFIRM)))) printf(ouptut);
+  else printf("Recieved from server: %s\nConfirmation failed.", output);
+
+  return from_server;
 }
 
-int server_handshake(int *fd){
+//returns to_client
+int server_handshake(int *from_client){
+  //1. server creates well known pipe and opens to read
   mkfifo(WKP,0644);
+  int from_client = open(WKP, O_RDONLY);
 
-  open(WKP);
-  
-  char buff[12];
-  read(WKP,buff,sizeof(buff));
+  //read private pipe name from client.
+  char* buff;
+  read(from_client,buff,sizeof(buff));
 
   buff[11]=0;
   write(buff,
--- pipe_networking.h
+++ pipe_networking.h
@@ -4,9 +4,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <unistd.h>
 
-#define MESSAGE_BUFFER_SIZE 64
+#define MESSAGE_BUFFER_SIZE 1024
 #define WKP "notouching"
+#define SERVER_CONFIRM "Connected to server. "
 
 int client_handshake(int *fd);
 int server_handshake(int *fd);
